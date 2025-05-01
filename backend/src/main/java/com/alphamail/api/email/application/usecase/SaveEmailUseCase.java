aae83243d82a1ccf5ab96657b13fd5f3f0b87c35
boriaegi <daeunchang@naver.com>
2025-05-01 15:34:06 +0900

    [S12P31A601-138] :sparkles: feat: 이메일을 저장하는 usecase를 따로 추가하라

---
+++ backend/src/main/java/com/alphamail/api/email/application/usecase/SaveEmailUseCase.java
@@ -0,0 +1,29 @@
+package com.alphamail.api.email.application.usecase;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alphamail.api.email.domain.entity.Email;
+import com.alphamail.api.email.domain.port.EmailSenderPort;
+import com.alphamail.api.email.domain.repository.EmailFolderRepository;
+import com.alphamail.api.email.domain.repository.EmailRepository;
+import com.alphamail.api.email.presentation.dto.SendEmailRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SaveEmailUseCase {
+
+	private final EmailRepository emailRepository;
+	private final EmailFolderRepository emailFolderRepository;
+
+	public Email execute(SendEmailRequest request, Integer userId) {
+
+		Integer sentFolderId = emailFolderRepository.getSentFolderId(userId);
+		Email email = Email.createForSending(request, userId, sentFolderId);
+		return emailRepository.save(email);
+
+	}
+}
