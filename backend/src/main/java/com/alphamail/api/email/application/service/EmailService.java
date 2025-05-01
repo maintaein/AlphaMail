a602061a3883064a25aeaa2f521b604bafc4b515
boriaegi <daeunchang@naver.com>
2025-05-01 15:29:54 +0900

    [S12P31A601-138] :sparkles: feat: 이메일 전송 상태 저장을 분리시키고 하나의 service에서 usecase들을 모아 관리하도록 전송 기능을 개발하라

---
+++ backend/src/main/java/com/alphamail/api/email/application/service/EmailService.java
@@ -0,0 +1,33 @@
+package com.alphamail.api.email.application.service;
+
+import org.springframework.stereotype.Service;
+
+import com.alphamail.api.email.application.usecase.SaveEmailUseCase;
+import com.alphamail.api.email.application.usecase.SendEmailUseCase;
+import com.alphamail.api.email.application.usecase.UpdateEmailUseCase;
+import com.alphamail.api.email.domain.entity.Email;
+import com.alphamail.api.email.domain.entity.EmailStatus;
+import com.alphamail.api.email.presentation.dto.SendEmailRequest;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+	private final SaveEmailUseCase saveEmailUseCase;
+	private final SendEmailUseCase sendEmailUseCase;
+	private final UpdateEmailUseCase updateEmailUseCase;
+
+	public void sendEmail(SendEmailRequest request, Integer userId) {
+		Email email = saveEmailUseCase.execute(request, userId);
+
+		try {
+			sendEmailUseCase.execute(email);
+			updateEmailUseCase.execute(email.getEmailId(), EmailStatus.SENT);
+
+		}catch (Exception e) {
+			updateEmailUseCase.execute(email.getEmailId(), EmailStatus.FAILED);
+			throw e;
+		}
+	}
+
+}
---
+++ backend/src/main/java/com/alphamail/api/email/application/usecase/UpdateEmailUseCase.java
@@ -0,0 +1,21 @@
+package com.alphamail.api.email.application.usecase;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alphamail.api.email.domain.entity.Email;
+import com.alphamail.api.email.domain.entity.EmailStatus;
+import com.alphamail.api.email.domain.repository.EmailRepository;
+import lombok.AllArgsConstructor;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class UpdateEmailUseCase {
+	private EmailRepository emailRepository;
+
+	public Email execute(Integer emailId, EmailStatus status) {
+		return emailRepository.updateStatus(emailId, status);
+
+	}
+}
