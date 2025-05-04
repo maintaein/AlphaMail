997a07fdf327712f2854e70e6b5493853fafce32
박종원 <jongwon990806@gmail.com>
2025-05-04 15:48:03 +0900

    [S12P31A601-185] :sparkles: feat: email domain에서 email을 통해서 userID를 가져오기 위한 user domain의 port 추가

---
+++ backend/src/main/java/com/alphamail/api/user/infrastructure/adapter/LoadUserAdapter.java
@@ -0,0 +1,25 @@
+package com.alphamail.api.user.infrastructure.adapter;
+
+import org.springframework.stereotype.Component;
+
+import com.alphamail.api.user.application.usecase.port.LoadUserPort;
+import com.alphamail.api.user.domain.repository.UserRepository;
+import com.alphamail.api.user.domain.valueobject.UserId;
+import com.alphamail.common.exception.ErrorMessage;
+import com.alphamail.common.exception.NotFoundException;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class LoadUserAdapter implements LoadUserPort {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserId loadUserIdByEmail(String email) {
+		return userRepository.findByEmail(email)
+			.orElseThrow(() -> new NotFoundException(ErrorMessage.MEMBER_NOT_FOUND))
+			.getId();
+	}
+}
