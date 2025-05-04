ccdb3e91636617842943a21d547ad9d9dc769efb
박종원 <jongwon990806@gmail.com>
2025-05-04 15:48:55 +0900

    [S12P31A601-185] :sparkles: feat: User Email만을 가지고 User를 조회하는 기능추가

--- backend/src/main/java/com/alphamail/api/user/domain/repository/UserRepository.java
+++ backend/src/main/java/com/alphamail/api/user/domain/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.alphamail.api.user.domain.repository;
 
+import java.util.Optional;
+
 import com.alphamail.api.user.domain.entity.User;
 
 public interface UserRepository {
+
+	Optional<User> findByEmail(String email);
 
 	User findById(Integer id);
 
