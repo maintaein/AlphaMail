88433123e43f4533b734f8e3aed67c8a3e887536
박종원 <jongwon990806@gmail.com>
2025-05-05 11:56:42 +0900

    [S12P31A601-186] 🔧 chore: AWS S3 설정 추가

--- backend/src/main/java/com/alphamail/common/config/AwsConfig.java
+++ backend/src/main/java/com/alphamail/common/config/AwsConfig.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 
@@ -37,6 +41,13 @@ public class AwsConfig {
 			.build();
 	}
 
+	@Bean
+	public AmazonS3 s3Client() {
 
+		return AmazonS3ClientBuilder.standard()
+			.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+			.withRegion(Regions.fromName(region))
+			.build();
+	}
 
 }
