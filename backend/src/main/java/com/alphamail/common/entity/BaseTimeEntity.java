4dc87105fbb31dd7fbfe4b551df392f67ac07a4d
daeun <daeunchang@naver.com>
2025-05-05 12:29:25 +0900

    [S12P31A601-188] :sparkles: feat: 공통으로 사용할 수 있는 BaseTimeEntity룰 추가하라

---
+++ backend/src/main/java/com/alphamail/common/entity/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package com.alphamail.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+import lombok.Getter;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseTimeEntity {
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false)
+	protected LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+}
