3c63ff410caf082ef1374b86b8a415cebf84209f
박도아 <sk05432@naver.com>
2025-05-12 18:35:49 +0900

    [S12P31A601-198] :recycle: refactor: 임시 일정 mapper를 수정해라

--- backend/src/main/java/com/alphamail/api/assistants/infrastructure/mapper/TemporaryScheduleMapper.java
+++ backend/src/main/java/com/alphamail/api/assistants/infrastructure/mapper/TemporaryScheduleMapper.java
@@ -9,11 +9,11 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring", uses = {EntityReferenceFinder.class})
 public interface TemporaryScheduleMapper {
 
+    @Mapping(source = "id", target = "temporaryScheduleId")
     @Mapping(source = "user.userId", target = "userId")
-    @Mapping(target = "id", source = "temporaryScheduleId")
     TemporarySchedule toDomain(TemporaryScheduleEntity entity);
 
-    @Mapping(target = "temporaryScheduleId", source = "id")
+    @Mapping(source = "temporaryScheduleId", target = "id")
     @Mapping(source = "userId", target = "user", qualifiedByName = "toUserEntity")
     TemporaryScheduleEntity toEntity(TemporarySchedule domain);
 }
