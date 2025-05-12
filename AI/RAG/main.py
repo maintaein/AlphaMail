1cfc7d30bcb9fda51ba3e7436ebd704322428a80
박종원 <jongwon990806@gmail.com>
2025-05-12 16:07:53 +0900

    [S12P31A601-291] :sparkles: Feat: db 저장용 디렉터리 추가

--- AI/RAG/main.py
+++ AI/RAG/main.py
@@ -8,6 +8,11 @@ app.include_router(email_router)
 
 if __name__ == "__main__":
 
+    
+    # db 저장용 디렉토리
+    os.makedirs("./db", exist_ok=True)
+
+
     #FastAPI 앱을 실행하는 ASGI 서버
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
