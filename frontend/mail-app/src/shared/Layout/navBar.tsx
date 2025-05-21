3c67a41cf723b1dddd2cae66fbbb65f066edefbf
unknown <yoonji0624@naver.com>
2025-05-21 10:22:34 +0900

    [S12P31A601-356] :art: style: navbar 버튼 호버시 색상을 통일하라

--- frontend/mail-app/src/shared/Layout/navBar.tsx
+++ frontend/mail-app/src/shared/Layout/navBar.tsx
@@ -87,7 +87,7 @@ export const NavBar = () => {
             <div className="p-4 flex justify-start">
                 <button
                     onClick={toggleCollapse}
-                    className="p-1 hover:bg-[#3E99C6] rounded text-white"
+                    className="p-1 hover:bg-[#5B98FB] rounded text-white"
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <line x1="3" y1="12" x2="21" y2="12"></line>
@@ -221,14 +221,14 @@ export const NavBar = () => {
                     !isCollapsed && contentVisible ? (
                         <button
                             onClick={handleLogoutClick}
-                            className="w-full py-3 text-white border border-white rounded-md hover:bg-[#3E99C6] transition-colors"
+                            className="w-full py-3 text-white border border-white rounded-md hover:bg-[#5B98FB] transition-colors"
                         >
                             <Typography variant="titleSmall" color="text-white">로그아웃</Typography>
                         </button>
                     ) : (
                         <button
                             onClick={handleLogoutClick}
-                            className="w-full flex justify-center py-3 text-white border border-white rounded-md hover:bg-[#3E99C6] transition-colors"
+                            className="w-full flex justify-center py-3 text-white border border-white rounded-md hover:bg-[#5B98FB] transition-colors"
                         >
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
